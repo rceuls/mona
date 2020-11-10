9d986ba7ae27094ee1f0d92f37dd86eb2e077b7e
Raf Ceuls <rceuls@gmail.com>
2020-11-10 21:19:54 +0100

    :lock: added security

--- Mona/Startup.cs
+++ Mona/Startup.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BlazorStrap;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -32,6 +35,73 @@ namespace Mona
             services.AddDbContext<ReportDataContext>(options => options.UseSqlite(Configuration["CONNECTION_STRING"]));
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddAuthentication(options =>
+  {
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+  })
+  .AddCookie()
+  .AddOpenIdConnect("Auth0", options =>
+  {
+    // Set the authority to your Auth0 domain
+    options.Authority = $"https://{Configuration["AUTH_DOMAIN"]}";
+
+    // Configure the Auth0 Client ID and Client Secret
+    options.ClientId = Configuration["AUTH_CLIENT_KEY"];
+    options.ClientSecret = Configuration["AUTH_CLIENT_SECRET"];
+
+    // Set response type to code
+    options.ResponseType = "code";
+
+    // Configure the scope
+    options.Scope.Clear();
+    options.Scope.Add("openid");
+    options.Scope.Add("email");
+
+    // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
+    // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+    options.CallbackPath = new PathString("/callback");
+
+    // Configure the Claims Issuer to be Auth0
+    options.ClaimsIssuer = "Auth0";
+
+    options.Events = new OpenIdConnectEvents
+    {
+      // handle the logout redirection
+      OnRedirectToIdentityProviderForSignOut = (context) =>
+        {
+          var logoutUri = $"https://{Configuration["AUTH_DOMAIN"]}/v2/logout?client_id={Configuration["AUTH_CLIENT_KEY"]}";
+
+          var postLogoutUri = context.Properties.RedirectUri;
+          if (!string.IsNullOrEmpty(postLogoutUri))
+          {
+            if (postLogoutUri.StartsWith("/"))
+            {
+              // transform to absolute
+              var request = context.Request;
+              postLogoutUri = $"{request.Scheme}://{request.Host}" + request.PathBase + postLogoutUri;
+            }
+            logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+          }
+
+          context.Response.Redirect(logoutUri);
+          context.HandleResponse();
+
+          return Task.CompletedTask;
+        }
+    };
+  });
+
+
             services.AddSingleton<AwsS3Uploader>();
             services.AddTransient<ReportService>();
         }
@@ -52,6 +122,9 @@ namespace Mona
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseRouting();
 
